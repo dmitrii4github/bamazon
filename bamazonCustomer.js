8a6c5348c1ffd503e4d41e07c288e071f8633fc8
Dmitrii Zakharov <dzakharov@splunk.com>
2018-10-25 15:41:39 -0700

    Serialized output to screen

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,8 @@
+if (process.stdout._handle) process.stdout._handle.setBlocking(true);
+
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var con = require('console');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,13 +20,48 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+  con.warn("connected as id " + connection.threadId + "\n");
+  inquire();
+
 });
 
-console.log("These are available items:");
+
+
+
+
+
+
+function inquire() {
+
+
 connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
-    console.log(res);
+    con.log("These are available items:");
+    con.log(JSON.stringify(res[0]));
+    con.log("\n");
+    con.log("Done listing products.");
+
+    // Create a "Prompt" with a series of questions.
+inquirer
+.prompt([
+    // Here we give the user a list to choose from.
+    { 
+      type: "id",
+      message: "What is item's id?",
+      name: "item_id",
+    }
+  ])
+  .then(function(inquirerResponse) {
+      con.log("You are requesting item: " + inquirerResponse.item_id);
+      var item_id = inquirerResponse.item_id;
+
+    });
+
 });
+
+
+
+
+}
 
